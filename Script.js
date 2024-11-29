d530769c5f65418e1c6340854f39e300948094ae
Nirmal Gusain <155016618+Nirmal-Gusain@users.noreply.github.com>
2024-11-29 23:34:57 +0530

    Form Validation

--- Script.js
+++ Script.js
@@ -21,40 +21,48 @@ let usernamePattern = /^[a-zA-Z0-9_-]{3,20}$/;
 let emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 let passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+={}\[\]:;"'<>?,./\\|-]).{8,}$/
 
+let userError =document.querySelector(".user")
+let emailError =document.querySelector(".email")
+let passError =document.querySelector(".pass")
 
 function checkValidation(e) {
     e.preventDefault()
 
 
 
-    if(checkbox.checked){
+    if (checkbox.checked) {
 
         if (usernamePattern.test(username.value)) {
             username.style.color = "green"
-            console.log("Username is valid"); // Optional logging
-    
-        } else {
+            userError.style.right = "-280px"
+        } else if(username.value == ""){
+            userError.style.right = "110px"
+        }else{
             username.style.color = "red"
-    
+
         }
-    
-        if(emailPattern.test(email.value)){
-            email.style.color = "green" 
-            console.log("Email is valid"); // Optional logging
+        if (emailPattern.test(email.value)) {
+            email.style.color = "green"
+            emailError.style.right = "-280px"
+        } else if(email.value == ""){
+            emailError.style.right = "110px"
         }else{
-            email.style.color = "red" 
-    
+            email.style.color = "red"
+
         }
-        if(passwordPattern.test(password.value)){
-            password.style.color = "green" 
-            console.log("Email is valid"); // Optional logging
+        if (passwordPattern.test(password.value)) {
+            password.style.color = "green"
+            passError.style.right = "-280px"
+        } else if(password.value == ""){
+            passError.style.right = "110px"
         }else{
-            password.style.color = "red" 
-    
+            password.style.color = "red"
+
         }
-        
+
+       
     }
-    
-}    
+}
+
 
 
